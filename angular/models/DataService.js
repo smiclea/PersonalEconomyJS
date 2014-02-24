7c73ef420618f6389cbf0629a4da059f9e4b9756
smiclea <smiclea@maporama.com>
2014-02-24 18:05:37 +0200

    Updated background

--- angular/models/DataService.js
+++ angular/models/DataService.js
@@ -3,16 +3,16 @@
 (function () {
 	'use strict';
 	
-	Parse.initialize("Hd9RWTswKHj8gu26fPXAUtnrsf7WVE7SBRkzSPz7", "Fple4j4xH67vnC8CzqtbeXhTl9iCxeKvpx72N5X2");
-	
-	Parse.User.logIn('sergiuoxigen', '[]people', {
-		success: function (user) {
-			
-		},
-		error: function (user, error) {
-			
-		}
-	});
+//	Parse.initialize("Hd9RWTswKHj8gu26fPXAUtnrsf7WVE7SBRkzSPz7", "Fple4j4xH67vnC8CzqtbeXhTl9iCxeKvpx72N5X2");
+//	
+//	Parse.User.logIn('sergiuoxigen', '[]people', {
+//		success: function (user) {
+//			
+//		},
+//		error: function (user, error) {
+//			
+//		}
+//	});
 	
 	angular.module('appServices').factory('User', ['$resource', function ($resource) {
 		
